bcb1c55136262554860c0ebb661ba84331736d94
David <DavidSaidiza0613@outlook.com>
2024-09-12 21:07:53 -0500

    FixConnectionString

--- DasignoTest/obj/Debug/net8.0/DasignoTest.AssemblyInfo.cs
+++ DasignoTest/obj/Debug/net8.0/DasignoTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DasignoTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776b672c7dad01710da863869a6eff2c128c0ee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DasignoTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DasignoTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
